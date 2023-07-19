2d20baa86622a1119867171fbbc9f0ae82ad27f3
idil <idilcalislar@gmail.com>
2023-07-19 17:11:09 +0200

    50 pages added from the api

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { SWRConfig } from "swr";
 import { GlobalStyle } from "../styles/styles.js";
 import Quiz from "../movies-app/Quiz";
 import Layout from "../movies-app/Layout.js";
 import MovieDetail from "../movies-app/MovieDetail";
+import Pagination from "./Pagination";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   const [movies, setMovies] = useState([]);
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  const url = `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&language=en-US&with_genres=14`;
-  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  useEffect(() => {
+    const url = `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&language=en-US&page=${currentPage}`;
+    // const { data, error, isLoading } = useSWR(url, fetcher);
+    console.log("url", url);
+    async function fetchData() {
+      const data = await fetch(url);
+      const result = await data.json();
+      console.log("RESULT", result);
+      setData(result);
+    }
+    fetchData();
+  }, [currentPage, apiKey]);
+
   console.log("data", data);
 
-  if (error) return <div> failed to load!</div>;
-  if (isLoading) return <div>loading...</div>;
+  // if (error) return <div> failed to load!</div>;
+  // if (isLoading) return <div>loading...</div>;
+  console.log("currentPage", currentPage);
+  const handlePaginationClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   //log the data, call setmovies pass in the needed props, remove the key to .env//
 
@@ -26,6 +45,11 @@ export default function App({ Component, pageProps }) {
         <GlobalStyle />
         <Component {...pageProps} movies={data} setMovies={setMovies} />
         <Layout />
+        <Pagination
+          currentPage={currentPage}
+          totalPages={50}
+          onPageClick={handlePaginationClick}
+        />
       </SWRConfig>
     </>
   );
--- pages/movie-detail/index.js
+++ pages/movie-detail/index.js
@@ -1,11 +1,11 @@
 import MovieList from "../../movies-app/MovieList";
 
-export default function AllMovies({ movies, setMovies }) {
+export default function AllMovies({movies, setMovies }) {
   console.log("movies imn allmovies", movies);
   return (
     <>
       <h1>Movies</h1>
-      <MovieList movies={movies} setMovies={setMovies} />
+      <MovieList  movies={movies} setMovies={setMovies} />
     </>
   );
 }
